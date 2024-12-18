7bfa17de1655d898fe3a5ecdb164900713273523
Georgiy Rzhavin <vimagik@gmail.com>
2024-12-18 22:46:29 +0300

    Added unit test for menu

--- src/components/AppMenu.test.js
+++ src/components/AppMenu.test.js
@@ -1,10 +1,11 @@
-import { mount } from '@vue/test-utils'
+import { mount, DOMWrapper } from '@vue/test-utils'
 import { describe, expect, it} from 'vitest'
 import { createVuetify } from 'vuetify'
 import { createTestingPinia } from '@pinia/testing'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import AppMenu from './AppMenu.vue'
+import { useUserStore } from '@/stores/user'
 
 
 const vuetify = createVuetify({
@@ -15,8 +16,8 @@ const vuetify = createVuetify({
 global.ResizeObserver = require('resize-observer-polyfill')
 
 describe('Unit test for AppMenu', () => {
-    it('Have acronim', () => {
-        const wrapper = mount({
+    function buildWrapper() { 
+        return mount({
             template: '<v-layout><app-menu></app-menu></v-layout>'
         }, {
             props: {},
@@ -25,13 +26,60 @@ describe('Unit test for AppMenu', () => {
                     AppMenu,
                 },
                 plugins: [vuetify, createTestingPinia({
+                    stubActions: false,
                     initialState: {
-                        user: { username: 'RGM' },
-                    },
+                        user: {
+                            username: 'RGM'
+                         },
+                     },
                 })],
             }
         })
-        expect(wrapper.html()).toContain('RG')
+    }
+
+    it('Component exist', () => {
+        const wrapper = buildWrapper()
+        expect(wrapper.exists()).toBe(true)
+    })
+
+    it('Outputs the label', () => {
+        const wrapper = buildWrapper()
+        const avatar = wrapper.find('[data-test="avatar"]')
+        expect(avatar.html()).toContain('RG')
+    })
+
+    it('Menu close by default', () => {
+        buildWrapper()
+        const dom = new DOMWrapper(document.body);
+        const menu = dom.find('[data-test="menu"]')
+        expect(menu.exists()).toBe(false)
+    })
+
+    it('Menu opens by click', async () => {
+        const wrapper = buildWrapper()
+        const button = wrapper.find('[data-test="avatar"]')
+        await button.trigger('click')
+        const dom = new DOMWrapper(document.body);
+        const menu = dom.find('[data-test="menu"]')
+        expect(menu.exists()).toBe(true)
+    })
+
+    it('Menu have add item', async () => {
+        const wrapper = buildWrapper()
+        const button = wrapper.find('[data-test="avatar"]')
+        await button.trigger('click')
+        const dom = new DOMWrapper(document.body);
+        const menu = dom.find('[data-test="menu"]')
+        expect(menu.html()).toContain('Добавить товар')
+    })
+
+    it('Menu have logout', async () => {
+        const wrapper = buildWrapper()
+        const button = wrapper.find('[data-test="avatar"]')
+        await button.trigger('click')
+        const dom = new DOMWrapper(document.body);
+        const menu = dom.find('[data-test="menu"]')
+        expect(menu.html()).toContain('Выйти')
     })
 })
 
--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -9,15 +9,15 @@ const store = useUserStore()
         <v-menu>
             <template v-slot:activator="{ props }">
                 <v-avatar class="cursor" color="indigo" v-bind="props">
-                    <span class="text-h5">{{ store.acronym }}</span>
+                    <span data-test="avatar" class="text-h5">{{ store.acronym }}</span>
                 </v-avatar>
             </template>
-            <v-list>
+            <v-list data-test="menu">
                 <v-list-item to="/newproduct">
                     <v-list-item-title>Добавить товар</v-list-item-title>
                 </v-list-item>
                 <v-list-item @click="store.logout">
-                    <v-list-item-title>Выйти</v-list-item-title>
+                    <v-list-item-title data-test="logout">Выйти</v-list-item-title>
                 </v-list-item>
             </v-list>
         </v-menu>
